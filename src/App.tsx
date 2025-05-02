aa9861253c430a96a7e55ed234852a627b25634d
Siddhant Aggarwal <siddhant197@gmail.com>
2025-05-02 18:37:28 +0530

    chore: setup routing and testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Character List</div>} />
+      </Routes>
+    </Router>
   );
 }
 
