54038f3b24446519558b7a6451ce22e9444301ae
Siddhant Aggarwal <siddhant197@gmail.com>
2025-05-03 17:12:26 +0530

    refactor: enhance styles

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -2,7 +2,7 @@ import { CharacterCardProps } from '../types/props';
 
 function CharacterCard({ character }: CharacterCardProps) {
   return (
-    <div className="border p-4 rounded shadow hover:shadow-lg transition">
+    <div className="transition border border-gray-700 duration-200 ease-in-out hover:border-black hover:bg-gray-800 hover:shadow-md hover:scale-[1.02] cursor-pointer rounded-lg p-4">
       <h3 className="text-xl font-semibold">{character.name}</h3>
       <p className="text-sm">Gender: {character.gender}</p>
       <p className="text-sm">Home Planet: {character.homeworld}</p>
